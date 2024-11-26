b7a20dcaa18212d792aa1dda33cff4505d128332
AKorsik <70960132+anastasiyakorsik@users.noreply.github.com>
2024-11-26 11:05:31 +0300

    Update container_status

--- YNP_inf/src/common/container_status.py
+++ YNP_inf/src/common/container_status.py
@@ -10,18 +10,21 @@ class ContainerStatus():
 		self.short_id = None
 		self.full_id = None
 		self.get_id()
-		self.events = ['/events/{}/before_start',		#Вызывается как можно раньше после запуска контейнера и перед началом работы скриптов обработки данных
-				'/events/{}/on_progress',		#Вызывается периодически по мере обработки данных в контейнере
-				'/events/{}/on_error',	#Вызывается в случае возникновения ошибки в процессе обработки данных в контейнере
-				'/events/{}/before_end']	#Вызывается после окончания обработки данных в контейнере и сохранением выходных файлов в папке OUT_DIR и перед остановкой (удалением) контейнера
+		self.events = ['/events/{}/before_start',	#Вызывается как можно раньше после запуска контейнера и перед началом работы скриптов обработки данных
+				'/events/{}/on_progress',			#Вызывается периодически по мере обработки данных в контейнере
+				'/events/{}/on_error',				#Вызывается в случае возникновения ошибки в процессе обработки данных в контейнере
+				'/events/{}/before_end']			#Вызывается после окончания обработки данных в контейнере и сохранением выходных файлов в папке OUT_DIR и перед остановкой (удалением) контейнера
 
 	def get_short_id(self):
-		'''Получение краткого id контейнера, как имя хоста
+		'''Получение краткого id контейнера
+		Args:	-
+		Return:	-
 		'''
 		self.short_id = socket.gethostname()
 
 	def get_containers_from_docker(self)->dict:
-		'''Получение списка активных контейнеров от docker
+		'''Получение списка активных контейнеров от docker host
+		Args:	-
 		Return: словарь вида full_id:name
 		'''
 		result = subprocess.check_output('docker ps --format "{{.ID}}: {{.Names}}" --no-trunc', shell=True).decode("utf-8").split('\n')
@@ -34,6 +37,8 @@ class ContainerStatus():
 
 	def get_id(self, full:bool = True):
 		'''Получение полного id контейнера
+		Args:	full - флаг для получения полного id контейнера
+		Return:	-
 		'''
 		self.get_short_id()
 		if full:
@@ -45,54 +50,55 @@ class ContainerStatus():
 		
 	def post(self, url:str, data:dict = {}):
 		'''Шаблон post запроса
+		Args:	data - сообщение в формате json, данны для отправки
 		Return: ответ на запрос
 		'''
-		#print(url)
 		res = {}
 		try:
-			res = requests.post(url, json = data)
+			if self.host_web != '':
+				res = requests.post(url, json = data)
 		except Exception as e:
-			print("Error: " +str(e))
+			print("Error: " + str(e))
 		finally:
 			return res
 		
 	def post_status(self, status:int, data:dict = None):
 		'''Шаблон post запроса со статусом
-		Args: 	status - № статуча из events
-				data - сообщение
+		Args: 	status - № статуса из events,
+				data - сообщение в формате json, данны для отправки
 		Return: ответ на запрос
 		'''
 		return self.post(urljoin(self.host_web,self.events[status].format(self.full_id)),data)
 		
 	def post_start(self,data:dict = None):
 		'''Шаблон post запроса со статусом before_start
-		Args: 	data - сообщение пустое
+		Args: 	data - см. camino-ann-events_v2.md
 		Return: ответ на запрос
 		'''
 		return self.post_status(0, data)
 		
 	def post_progress(self,data = None):
 		'''Шаблон post запроса со статусом on_progress
-		Args: 	data - сообщение формата {'on_progress':'0.xx'} - процент выполнения в виде десятичной дроби
+		Args: 	data - см. camino-ann-events_v2.md
 		Return: ответ на запрос
 		'''
 		return self.post_status(1,data)
 		
 	def post_error(self,data = None):
 		'''Шаблон post запроса со статусом on_error
-		Args: 	data - сообщение формата {'on_error':'error text'} - текст ошибки
+		Args: 	data - см. camino-ann-events_v2.md
 		Return: ответ на запрос
 		'''
 		return self.post_status(2,data)
 		
 	def post_end(self,data = None):
 		'''Шаблон post запроса со статусом before_end
-		Args: 	data - сообщение формата {'out_files':['xxx.json', 'yyy.json']} - имена выходных файлов
+		Args: 	data - см. camino-ann-events_v2.md
 		Return: ответ на запрос
 		'''
 		return self.post_status(3,data)
 
-def get_id():
+def get_id()->str:
 	'''Получение полного id контейнера
 	'''
 	full_id = ""
