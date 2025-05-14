65896c5111017551f73f177febe682224b9205ee
a.korsik <korsik.ae@phystech.edu>
2025-05-14 20:49:00 +0300

    fix check_input.py

--- src/common/check_input.py
+++ src/common/check_input.py
@@ -7,18 +7,21 @@ from src.common.logger import py_logger
 def check_input_files_for_training_mode(input_json_files):
     if input_json_files:
         py_logger.info(f"Training program mode")
+        check_files_result = False
         for json_file in input_json_files:
             full_json_path = os.path.join(INPUT_DATA_PATH, json_file)
             py_logger.info(f"Check current input JSON file: {full_json_path} for skeletons markup in")
             data = load_json(str(full_json_path))
+            check_files_result = False
             if data:
                 input_data = data['files'][0]
                 skeleton_nodes = input_data['file_chains'][0]['chain_markups'][0]['markup_path'].get('nodes', [])
                 if len(skeleton_nodes) > 0:
-                    return True
+                    check_files_result = True
                 else:
-                    return False
+                    check_files_result = False
             else:
-                return False
+                check_files_result = False
+        return check_files_result
     else:
         return False
